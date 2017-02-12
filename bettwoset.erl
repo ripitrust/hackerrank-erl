7bf909b85fa30144802e54bfeb6406e4f3e6b021
Chrimm <ripitrust@gmail.com>
2017-02-13 02:37:53 +0800

    add bettwoset

---
+++ bettwoset.erl
@@ -0,0 +1,43 @@
+-module(bettwoset).
+-export([smaller_set/2, bigger_set/2, solve/4]).
+
+
+
+
+solve(M, N, A, B) ->
+
+    Head = lists:nth(M, A),
+    Tail = lists:nth(N, B),
+
+    if Head < Tail ->
+        Range = lists:seq(Head, Tail),
+        length(lists:filter( fun(X) -> X end,  [ test(A, B, X) || X <- Range ]));
+
+    Head >= Tail -> 0
+
+    end.
+
+test(A, B, Num) ->
+
+    SmRes = smaller_set(A, Num),
+    BgRes = bigger_set(B, Num),
+
+    if SmRes and BgRes -> true;
+        true -> false
+    end.
+
+smaller_set([], _) -> true;
+smaller_set([A | Rest], Num) ->
+
+    if Num rem A == 0 -> smaller_set(Rest, Num);
+       true -> false
+    end.
+
+bigger_set([], _) -> true;
+bigger_set([A | Rest], Num) ->
+
+    if A rem Num == 0 -> bigger_set(Rest, Num);
+       true -> false
+    end.
+
+    
